571b0caac38dcf43e581717b04f2dee6a65d23b4
hushin <nnnnot+github@gmail.com>
2020-09-10 00:20:29 +0900

    add 003

---
+++ src/003.js
@@ -0,0 +1,18 @@
+const goal = 600851475143
+const primeFactors = [] // debug用
+let i = 2
+let next = goal
+let maxPrime = i
+
+while (i <= next) {
+  if (next % i === 0) {
+    maxPrime = i
+    primeFactors.push(i)
+    next = next / i
+    while (next % i === 0) {
+      next = next / i
+    }
+  }
+  i++
+}
+console.log(primeFactors, maxPrime)
