eca08cb12508671066283287b7fe2b672020080f
hushin <nnnnot+github@gmail.com>
2020-09-14 14:20:04 +0900

    fix 005

--- src/005.js
+++ src/005.js
@@ -1,24 +1,39 @@
 const isPrime = (x) => {
   if (x < 2) return false
+  if (x === 2) return true
   let i = 2
-  while (i < x) {
+  const sqrt = Math.ceil(Math.sqrt(x))
+  while (i <= sqrt) {
     if (x % i === 0) return false
     i++
   }
   return true
 }
 
+const maxPower = (prime, max) => {
+  let result = prime
+  let exponent = 2
+  let next = prime ** exponent
+  while (next < max) {
+    result = next
+    exponent++
+    next = prime ** exponent
+  }
+  return result
+}
+
 const calc = (x) => {
   if (x < 2) return NaN
   let result = 1
   let i = 2
-  let j
   while (i <= x) {
-    if (isPrime(i)) result *= i
+    if (isPrime(i)) result *= maxPower(i, x)
     i++
   }
 
   return result
 }
 
-console.log(calc(10))
+// console.log(maxPower(2, 10))
+// console.log(calc(10))
+console.log(calc(20))
