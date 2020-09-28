fe88aa751463ca1552249462fa08e307d17d66a8
hushin <nnnnot+github@gmail.com>
2020-09-28 23:39:07 +0900

    fix 012

--- src/012.js
+++ src/012.js
@@ -1,42 +1,25 @@
 // 三角数を求める
-const triangleNumbers = [0, 1]
-const getTriangleNum = (x) => {
-  const cache = triangleNumbers[x]
-  if (cache) return cache
-  const result = getTriangleNum(x - 1) + x
-  triangleNumbers[x] = result
-  return result
-}
+const getTriangleNum = (x) => (x * (x + 1)) / 2
 
 // 約数の数を求める
 const getDivisorsLength = (x) => {
-  if (x === 1) return 1
-  let divisorsLength = 1
-  let minDivisor = 2
-  let isDivisible = false
-  for (let i = 2; i <= x / minDivisor; i++) {
+  let divisorsLength = 0
+  const sqrt = Math.sqrt(x)
+  for (let i = 1; i <= x / sqrt; i++) {
     if (x % i === 0) {
-      isDivisible = true
-      divisorsLength++
-    } else if (!isDivisible) {
-      minDivisor = i
+      divisorsLength += 2
     }
   }
-  divisorsLength++
   return divisorsLength
 }
 
 // console.log(getDivisorsLength(28))
-// console.log(getTriangleNum(7), triangleNumbers)
+// console.log(getTriangleNum(7))
 
 let i = 1
 while (true) {
   const triangleNum = getTriangleNum(i)
   const divisorsLength = getDivisorsLength(triangleNum)
-  if (divisorsLength > 200) {
-    // 計算時間が長いので途中経過を確認
-    console.log(divisorsLength, triangleNum, i)
-  }
   if (divisorsLength > 500) {
     console.log(triangleNum)
     break
